e3bf39eb69fa7406a6a803dfa7c1edd6110f0d56
daeun <3v3r.alp@gmail.com>
2025-07-17 15:50:08 +0900

    반응형 수정

---
+++ scripts/index.js
@@ -0,0 +1,134 @@
+/* 1행 배너 */
+
+const topSwiper = new Swiper('.top_banner',{
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    slidesPerView:1,
+    autoplay:{delay:6000,},
+    speed:2500,
+    loop:true,
+});
+
+/* 추천 조합 */
+const swiper = new Swiper(".look_book_swiper", {
+    direction: 'horizontal', // 가로 방향 슬라이드
+    slidesPerView: 'auto',   // 자동 슬라이드 너비 설정
+    spaceBetween: 20,        // 슬라이드 간 간격
+    freeMode: true,          // 드래그로 자유롭게 움직이기
+    watchOverflow: true,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        draggable: true, // 드래그 가능하도록 설정
+        hide: false      // 항상 보이게
+    },
+});
+
+const newCategoryBtn = document.querySelectorAll('.category_btn_new button');
+const bestCategoryBtn = document.querySelectorAll('.category_btn_best button');
+const lookbookCategoryBtn = document.querySelectorAll('.category_btn_lookbook button');
+
+const newProductList = document.querySelectorAll('.new_product_wrap .product_list');
+const bestProductList = document.querySelectorAll('.best_right .product_best_right');
+const bestBigProduct = document.querySelectorAll('.big_img_product');
+const lookbookProductList = document.querySelectorAll('.look_book_swiper');
+const lookbookLeftImg = document.querySelectorAll('.look_book_left_img');
+
+
+
+
+/* 신상 */
+
+for(let i=0; i<3; i++){
+    newCategoryBtn[i].addEventListener('click',()=>{
+        if(newCategoryBtn[i].style.color == '#fff'){
+            newCategoryBtn[i].style.backgroundColor = '#fff';
+            newCategoryBtn[i].style.color = '#000';
+        }else{
+            for(let j=0; j<3; j++){
+                newCategoryBtn[j].style.backgroundColor = '#fff';
+                newCategoryBtn[j].style.color = '#000';
+            }
+            newCategoryBtn[i].style.backgroundColor = '#C86734';
+            newCategoryBtn[i].style.color = '#fff';
+        }
+    });
+}
+
+newCategoryBtn.forEach((obj,ind)=>{
+    obj.addEventListener('click',()=>{
+        for(let i of newProductList){i.style.display = 'none'}
+        newProductList[ind].style.display = 'flex';
+    })
+    obj.addEventListener('click',()=>{
+        for(let i of newProductList){i.style.display = 'none'}
+        newProductList[ind].style.display = 'flex';
+    })
+})
+
+
+
+/* 베스트 */
+for(let i=0; i<3; i++){
+    bestCategoryBtn[i].addEventListener('click',()=>{
+        if(bestCategoryBtn[i].style.color == '#fff'){
+            bestCategoryBtn[i].style.backgroundColor = '#fff';
+            bestCategoryBtn[i].style.color = '#000';
+        }else{
+            for(let j=0; j<3; j++){
+                bestCategoryBtn[j].style.backgroundColor = '#fff';
+                bestCategoryBtn[j].style.color = '#000';
+            }
+            bestCategoryBtn[i].style.backgroundColor = '#C86734';
+            bestCategoryBtn[i].style.color = '#fff';
+        }
+    });
+}
+
+bestCategoryBtn.forEach((obj,ind)=>{
+    obj.addEventListener('click',()=>{
+        for(let i of bestProductList){i.style.display = 'none'}
+        bestProductList[ind].style.display = 'flex';
+        for(let i of bestBigProduct){i.style.display = 'none'}
+        bestBigProduct[ind].style.display = 'block';
+    })
+})
+
+/* 추천조합 */
+
+
+for(let i=0; i<3; i++){
+    lookbookCategoryBtn[i].addEventListener('click',()=>{
+        if(lookbookCategoryBtn[i].style.color == '#fff'){
+            lookbookCategoryBtn[i].style.backgroundColor = '#fff';
+            lookbookCategoryBtn[i].style.color = '#000';
+        }else{
+            for(let j=0; j<3; j++){
+                lookbookCategoryBtn[j].style.backgroundColor = '#fff';
+                lookbookCategoryBtn[j].style.color = '#000';
+            }
+            lookbookCategoryBtn[i].style.backgroundColor = '#C86734';
+            lookbookCategoryBtn[i].style.color = '#fff';
+        }
+    });
+}
+
+lookbookCategoryBtn.forEach((obj,ind)=>{
+    obj.addEventListener('click',()=>{
+        for(let i of lookbookProductList){i.style.display = 'none'}
+        lookbookProductList[ind].style.display = 'flex';
+    })
+    obj.addEventListener('click',()=>{
+        for(let i of lookbookLeftImg){i.style.display = 'none'}
+        lookbookLeftImg[ind].style.display = 'grid';
+    })
+})
+
+
+
+
